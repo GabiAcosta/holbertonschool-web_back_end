b9f0bbc7d4bd465eedc3eff3a182ba45b7c5b8a1
Gabriel <acostagabriel1311@gmai.com>
2023-08-28 06:08:21 -0700

    first commit

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Add Function
+
+This module defines a function that adds two floating-point numbers.
+"""
+def add(a: float, b:float) -> float:
+    """Adds two floating-point numbers."""
+    return a + b
