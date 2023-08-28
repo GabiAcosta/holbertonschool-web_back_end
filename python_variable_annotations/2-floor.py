ed7cc6d591765279390432fdd350b0060c21cbcf
Gabriel <acostagabriel1311@gmai.com>
2023-08-28 06:20:56 -0700

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Floor Function
+
+This module defines a function that calculates the floor of a
+floating-point number.
+"""
+
+
+def floor(n: float) -> int:
+    """Calculates the floor of a floating-point number."""
+    return int(n)
