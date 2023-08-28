8b5f659314a6e1b9d47a266f637ec0c12e82da84
Gabriel <acostagabriel1311@gmai.com>
2023-08-28 06:11:15 -0700

    pycodestyle fix

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Add Function
 
 This module defines a function that adds two floating-point numbers.
 """
-def add(a: float, b:float) -> float:
+
+
+def add(a: float, b: float) -> float:
     """Adds two floating-point numbers."""
     return a + b
