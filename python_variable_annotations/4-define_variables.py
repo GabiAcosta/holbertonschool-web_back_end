75261cb833adea0c50a072bef0a224606f0edc0b
Gabriel <acostagabriel1311@gmai.com>
2023-08-28 06:27:14 -0700

    task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Variable Annotations
+
+This module defines and documents several variables with type annotations.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
