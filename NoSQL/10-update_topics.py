d5dd82c8ba5f450d996935860e54f9d1da616f61
GabiAcosta <acostagabriel1311@gmail.com>
2023-09-05 12:08:59 -0700

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module defines a function for updating the topics field of a school
+document in a MongoDB collection.
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Update the topics field of a school document in a MongoDB collection.
+    """
+    mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
