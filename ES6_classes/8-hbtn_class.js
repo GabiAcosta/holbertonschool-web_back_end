6df85ac32311e475fbe3c47e1fef256551789b17
Gabriel <acostagabriel1311@gmai.com>
2023-09-14 10:36:33 -0700

    task 8

--- ES6_classes/8-hbtn_class.js
+++ ES6_classes/8-hbtn_class.js
@@ -3,12 +3,22 @@ export default class HolbertonClass {
     this._size = size;
     this._location = location;
   }
-
+  /* valid methods but [Symbol.toPrimitive] is more accurate in this case
   valueOf() {
     return this._size;
   }
 
   toString() {
     return this._location;
+  } */
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return this;
   }
 }
