3829080c7d1c0f5fed3f39a96f3733d4432e3dea
Gabriel <acostagabriel1311@gmai.com>
2023-10-02 11:38:16 -0700

    task 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((values) => ({
+      status: values[0].status,
+      value: values[0].value,
+    }));
+}
