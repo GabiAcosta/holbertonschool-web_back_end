23b6c33763aecf1e034f1075c87b0b9dbe6a90be
Gabriel <acostagabriel1311@gmai.com>
2023-10-02 11:44:08 -0700

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,8 +3,14 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
-    .then((values) => ({
-      status: values.status,
-      value: values.value,
-    }));
+    .then((values) => ([
+      {
+        status: values[0].status,
+        value: values[0].value,
+      },
+      {
+        status: values[1].status,
+        value: values[1].value,
+      },
+    ]));
 }
