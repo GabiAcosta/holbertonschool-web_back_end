2c2034e031748cd52b177d2172072213482c8ccb
GabiAcosta <acostagabriel1311@gmail.com>
2023-10-04 05:41:03 -0700

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   if (startString === '') return '';
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  const cleanString = filteredValues.map((value) => value.substring(startString.length)).join('-');
+  const filteredValues = [...set].filter((value) => typeof value === 'string' && value.startsWith(startString));
+  const cleanString = filteredValues.map((value) => value.slice(startString.length)).join('-');
   return cleanString;
 }
